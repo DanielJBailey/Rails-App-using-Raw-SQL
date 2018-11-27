1633defb87805040b2f8d9c29e387d3d943f36d5
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-11-27 13:12:34 -0700

    finish building controller and model methods for raw SQL

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,49 +3,41 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   def index
-    @customers = Customer.all
+    @customers = Customer.all_customers(current_user.id)
   end
 
   def show
   end
 
   def new
-    @customer = Customer.new
+    @customer = current_user.customers.new
   end
 
   def create
-    @customer = Customer.new(customer_params)
-    if @customer.save
-      redirect_to customers_path
-    else
-      render :new
-    end
+    Customer.create_customer(customer_params, current_user.id)
+    redirect_to customers_path
   end
 
   def edit
   end
 
   def update
-    @customer = Customer.new(customer_params)
-    if @customer.update
-      redirect_to customers_path
-    else
-      render :edit
-    end
+    Customer.update_customer(customer_params, @customer.id, current_user.id)
+    redirect_to @customer
   end
 
   def destroy
-    @customer.destroy
+    Customer.delete_customer(current_user.id, @customer)
     redirect_to customers_path
   end
 
   private
 
   def set_customer
-    @customer = Customer.find(params[:id])
+    @customer = Customer.single_customer(current_user.id, params[:id])
   end
 
   def customer_params
-    Params.require(:customer).permit(:first_name, :last_name, :phone, :email)
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone )
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,80 @@
 class Customer < ApplicationRecord
   belongs_to :user
+
+  # Instance Method
+  def full_name
+    #"#{self.first_name} #{self.last_name}"
+    customer = Customer.find_by_sql(["
+        SELECT 
+        first_name, last_name 
+        from customers AS c
+        WHERE c.id = ?
+      ", self.id]).first
+      "#{customer.first_name} #{customer.last_name}"
+  end
+
+  # Model Method
+  # Select all customer records for user
+  def self.all_customers(user_id)
+    Customer.find_by_sql("
+      SELECT *
+      FROM customers AS c
+      WHERE c.user_id = #{user_id}
+    ")
+  end
+
+  # Select a single customer for user
+  def self.single_customer(user_id, customer_id)
+    Customer.find_by_sql(["
+        SELECT * 
+        FROM customers AS c
+        WHERE c.user_id = ?
+        AND c.id = ?
+      ", user_id, customer_id]).first
+  end
+
+  # Create a single customer
+  def self.create_customer(params, user_id)
+    Customer.find_by_sql(["
+        INSERT INTO customers (first_name, last_name, email, phone, user_id, created_at, updated_at)
+        VALUES (:first, :last, :email, :phone, :user_id, :created_at, :updated_at)
+      ", {
+        first: params[:first_name],
+        last: params[:last_name],
+        email: params[:email],
+        phone: params[:phone],
+        user_id: user_id,
+        created_at: DateTime.now,
+        updated_at: DateTime.now
+      }])
+  end
+
+  # Update a single customer
+  def self.update_customer(params, customer_id, user_id)
+    Customer.find_by_sql(["
+        UPDATE customers AS c
+        SET first_name = ?, last_name = ?, email = ?, phone = ?, updated_at = ?
+        WHERE c.id = ? AND c.user_id = ?
+      ", params[:first_name], params[:last_name], params[:email], params[:phone], DateTime.now, customer_id, user_id])
+  end
+
+  # Delete a single customer
+  def self.delete_customer(user_id, customer_id)
+    Customer.find_by_sql(["
+        DELETE FROM customers AS c
+        WHERE c.user_id = ? AND c.id = ?
+      ", user_id, customer_id])
+  end
 end
+
+
+
+# IF USER ID IS A STRING OR USER NAME IS IN STRING FORMAT, YOU HAVE TO PASS IT IN AS FOLLOWS
+# # Model Method
+# def self.all_customers(user_id)
+#   Customer.find_by_sql(["
+#       SELECT *
+#       FROM customers AS c
+#       WHERE c.user_id = ?
+#     ", user_id])
+# end
